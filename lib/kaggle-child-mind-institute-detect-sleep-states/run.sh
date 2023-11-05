f32de70f53ca6d371d6310c14ef7dfa10e9993cb
jumtras <data0science0acc@gmail.com>
2023-11-05 07:28:40 +0000

    custom sh

--- lib/kaggle-child-mind-institute-detect-sleep-states/run.sh
+++ lib/kaggle-child-mind-institute-detect-sleep-states/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-python3 run/train.py exp_name=exp004-fold0 split=fold_0
-python3 run/train.py exp_name=exp004-fold1 split=fold_1
-python3 run/train.py exp_name=exp004-fold2 split=fold_2
-python3 run/train.py exp_name=exp004-fold3 split=fold_3
-python3 run/train.py exp_name=exp004-fold4 split=fold_4
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold0 split=fold_0
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold1 split=fold_1
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold2 split=fold_2
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold3 split=fold_3
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold4 split=fold_4
