a62656a42c2c97c8e463936790a20c8df8b9bf2e
jumtras <data0science0acc@gmail.com>
2023-11-05 13:19:24 +0000

    feature: for gcp

--- lib/kaggle-child-mind-institute-detect-sleep-states/run.sh
+++ lib/kaggle-child-mind-institute-detect-sleep-states/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold0 split=fold_0
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold1 split=fold_1
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold2 split=fold_2
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold3 split=fold_3
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold4 split=fold_4
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold0 split=fold_0
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold1 split=fold_1
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold2 split=fold_2
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold3 split=fold_3
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 python3 run/train.py exp_name=manet-fold4 split=fold_4
