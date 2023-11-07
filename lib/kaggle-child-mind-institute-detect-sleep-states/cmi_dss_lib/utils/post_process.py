a10b22d2b7e1b3ad6ba7aeeb737c1fbdf1e9c405
jumtras <data0science0acc@gmail.com>
2023-11-07 07:57:42 +0000

    feature : post_process add sleep

--- lib/kaggle-child-mind-institute-detect-sleep-states/cmi_dss_lib/utils/post_process.py
+++ lib/kaggle-child-mind-institute-detect-sleep-states/cmi_dss_lib/utils/post_process.py
@@ -53,3 +53,70 @@ def post_process_for_seg(
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
     return sub_df
+
+def post_process_for_seg_v1(
+    keys: list[str], preds: np.ndarray, score_th: float = 0.01, distance: int = 5000,th_hour:int = 6
+) -> pl.DataFrame:
+    """make submission dataframe for segmentation task
+
+    Args:
+        keys (list[str]): list of keys. key is "{series_id}_{chunk_id}"
+        preds (np.ndarray): (num_series * num_chunks, duration, 3)
+        score_th (float, optional): threshold for score. Defaults to 0.5.
+
+    Returns:
+        pl.DataFrame: submission dataframe
+    """
+    th_step = 12*60*th_hour # th_hour h
+    series_ids = np.array(list(map(lambda x: x.split("_")[0], keys)))
+    unique_series_ids = np.unique(series_ids)
+
+    records = []
+    for series_id in unique_series_ids:
+        series_idx = np.where(series_ids == series_id)[0]
+        this_series_preds = preds[series_idx].reshape(-1, 3)
+
+        for i, event_name in enumerate(["onset", "wakeup"]):
+            this_event_preds = this_series_preds[:, i+1]
+            steps = find_peaks(this_event_preds, height=score_th, distance=distance)[0]
+            scores = this_event_preds[steps]
+            sleep_scores = this_series_preds[:,0][steps]
+            sleep_scores = []
+            for step in steps:
+                min_step = step-th_step if (step -th_step) >=0 else 0
+                max_step = step+th_step if (step +th_step) <=len(this_event_preds) else len(this_event_preds)
+
+                if event_name == "onset":
+                    sleep_scores.append(np.median(this_series_preds[:,0][step:max_step]))
+
+                if event_name == "wakeup":
+                    sleep_scores.append(np.median(this_series_preds[:,0][min_step:step]))
+            sleep_scores = np.array(sleep_scores)
+                
+
+            for step, score,sleep_score in zip(steps, scores,sleep_scores):
+                records.append(
+                    {
+                        "series_id": series_id,
+                        "step": step,
+                        "event": event_name,
+                        "score": score,
+                        "sleep_score": sleep_score,
+                    }
+                )
+
+    if len(records) == 0:  # 一つも予測がない場合はdummyを入れる
+        records.append(
+            {
+                "series_id": series_id,
+                "step": 0,
+                "event": "onset",
+                "score": 0,
+                "sleep_score": 0,
+            }
+        )
+
+    sub_df = pl.DataFrame(records).sort(by=["series_id", "step"])
+    row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
+    sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score", "sleep_score"])
+    return sub_df
