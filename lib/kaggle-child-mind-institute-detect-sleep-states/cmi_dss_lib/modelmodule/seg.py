d4f798fc6d86592dd0e8a3b0b092f34e9aba0a38
yugo.omura <yomura1008@gmail.com>
2023-11-19 02:08:16 +0900

    modified:   lib/kaggle-child-mind-institute-detect-sleep-states/cmi_dss_lib/modelmodule/seg.py 	new file:   lib/kaggle-child-mind-institute-detect-sleep-states/config/exp/55.yaml

--- lib/kaggle-child-mind-institute-detect-sleep-states/cmi_dss_lib/modelmodule/seg.py
+++ lib/kaggle-child-mind-institute-detect-sleep-states/cmi_dss_lib/modelmodule/seg.py
@@ -21,10 +21,10 @@ class SegChunkModule(BaseChunkModule):
         duration: int,
         model_save_dir_path: pathlib.Path | None = None,
     ):
+        super().__init__(cfg, val_event_df, duration, model_save_dir_path)
         self.model = cmi_dss_lib.models.common.get_model(
             cfg, feature_dim=feature_dim, n_classes=num_classes, num_time_steps=self.num_time_steps
         )
-        super().__init__(cfg, val_event_df, duration, model_save_dir_path)
 
     def forward(
         self, batch: dict[str : torch.Tensor], *, do_mixup=False, do_cutmix=False
